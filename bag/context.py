16fcc10082c01403ed967989406a3675961a16c2
roman rakic <rock.rakic@gmail.com>
2023-04-02 11:39:32 +0100

    add bag context proccessor and delivery logic

---
+++ bag/context.py
@@ -0,0 +1,31 @@
+from decimal import Decimal
+from django.conf import settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_TRESHOLD:
+        delivery = total = Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        free_delivery_delta = settings.FREE_DELIVERY_TRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+
+
+
+    context = {
+       'bag_items':bag_items,
+       'total': total,
+       'product_count': product_count,
+       'free_delivery_delta' : free_delivery_delta,
+       'free_delivery_treshold' : settings.FREE_DELIVERY_TRESHOLD,
+       'grand_total' : grand_total
+    }
+
+    return context
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.context.bag_contents',
             ],
         },
     },
@@ -157,6 +158,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_TRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
