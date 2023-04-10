90530b096fe5652d51463c5320b03f6faea94d1c
roman rakic <rock.rakic@gmail.com>
2023-04-10 11:39:54 +0100

    add checkout succes logic

--- checkout/models.py
+++ checkout/models.py
@@ -23,7 +23,7 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
    
-    def generate_order_number(self):
+    def _generate_order_number(self):
         """
         Generate a random unique order
         number using uuid
@@ -36,7 +36,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))['lineitem_total__sum']
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
