9481ea13bcd3843bfb6c16d5bbb7f2054bee0d6c
roman rakic <rock.rakic@gmail.com>
2023-04-11 23:13:56 +0100

    update checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -101,7 +101,7 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         
         )
-    # print(intent)
+    print(intent)
 
     order_form = OrderForm()
     if not stripe_public_key:
