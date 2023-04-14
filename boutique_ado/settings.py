f966b15d8e56a629a73e957af2013088019cd0e1
Roman Rakic <rock.rakic@gmail.com>
2023-04-14 12:49:06 +0100

    fix allowed hosts spelling

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-boutique-ci', 'localhost']
+ALLOWED_HOSTS = ['my-boutique-ci.herokuapp.com', 'localhost']
 
 
 # Application definition
