e7351b520e8f99e0f97c6db488431b2e7a06b9bb
Roman Rakic <rock.rakic@gmail.com>
2023-04-17 00:45:39 +0100

    css and templates cleanup

--- bag/views.py
+++ bag/views.py
@@ -12,25 +12,21 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """
-    add quantity of chosen product
-    to the shopping bag
-    """
-    product = Product.objects.get(pk=item_id)
+    """ Add a quantity of the specified product to the shopping bag """
+
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-
     bag = request.session.get('bag', {})
 
     if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(
-                    request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
